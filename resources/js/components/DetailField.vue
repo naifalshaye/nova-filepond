29bd1c10b72e008f9371de21e677a77190f0b922
Naif Alshaye <naif@naif.io>
2019-11-26 18:24:49 +0300

    PDF review and support Word and Excel

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -3,23 +3,76 @@
     <panel-item :field="field">
 
         <file-pond-wrapper
-            slot="value"
-            :disabled="true"
-            :allow-image-preview="!field.multiple"
-            :limit="field.value.length"
-            :field="field"/>
+                slot="value"
+                :disabled="true"
+                :allow-image-preview="!field.multiple"
+                :limit="field.value.length"
+                :field="field"
+                v-if="!pdf && !word && !excel"
+                />
+
+        <file-pond-wrapper2
+                slot="value"
+                :disabled="true"
+                :allow-image-preview="!field.multiple"
+                :limit="field.value.length"
+                :field="field"
+                v-if="pdf"
+        />
+
+        <file-pond-wrapper3
+                slot="value"
+                :disabled="true"
+                :allow-image-preview="!field.multiple"
+                :limit="field.value.length"
+                :field="field"
+                v-if="word"
+        />
+
+        <file-pond-wrapper4
+                slot="value"
+                :disabled="true"
+                :allow-image-preview="!field.multiple"
+                :limit="field.value.length"
+                :field="field"
+                v-if="excel"
+        />
+
 
     </panel-item>
+
 
 </template>
 
 <script>
 
     import FilePondWrapper from './FilePondWrapper'
+    import FilePondWrapper2 from './FilePondWrapper2'
+    import FilePondWrapper3 from './FilePondWrapper3'
+    import FilePondWrapper4 from './FilePondWrapper4'
 
     export default {
-        components: {FilePondWrapper},
-        props: ['resource', 'resourceName', 'resourceId', 'field']
+        components: {FilePondWrapper,FilePondWrapper2,FilePondWrapper3,FilePondWrapper4},
+        props: ['resource', 'resourceName', 'resourceId', 'field'],
+        data: function () {
+            return {
+                pdf: false,
+                word: false,
+                excel: false
+            }
+        },
+        mounted() {
+            var file = this.field.thumbnails[0];
+            var ext = file.substr(file.lastIndexOf('.') + 1);
+            if (ext == 'pdf') {
+                this.pdf = true;
+            }
+            else if (ext == 'doc' || ext == 'docx'){
+                this.word = true;
+            }
+            else if (ext == 'xl' || ext == 'xla' || ext == 'xls' || ext == 'xlsx'){
+                this.excel = true;
+            }
+        }
     }
-
 </script>
---
+++ resources/js/components/FilePondWrapper2.vue
@@ -0,0 +1,9 @@
+<template>
+    <iframe id="iframepdf" :src="this.field.thumbnails[0]" width="700" height="700"></iframe>
+</template>
+
+<script>
+    export default {
+        props: ['field'],
+    }
+</script>
